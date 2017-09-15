2dfb256638f70acdfd2ad12391d1feaed6e46915
Erinna Chen <erinna.chen@gmail.com>
2017-09-15 14:20:12 -0500

    Add deploy things

--- lib/doggo_bot.ex
+++ lib/doggo_bot.ex
@@ -12,7 +12,15 @@ defmodule DoggoBot do
       :world
 
   """
-  def hello do
-    :world
+  def main(_argv) do
+    try do
+      tweet = ExTwitter.user_timeline([screen_name: "dog_rates"])
+        |> Enum.find(fn(tweet) -> !tweet.retweeted && !tweet.in_reply_to_user_id && tweet.retweet_count > 1000 end)
+      if tweet do
+        ExTwitter.retweet(tweet.id)
+      end
+    rescue
+      ExTwitter.Error -> :error
+    end
   end
 end
